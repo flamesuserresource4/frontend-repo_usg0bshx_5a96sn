da2016b87b45b0139a8174b209b56a5eda2dda1f
Flames <vibe-coder@flames.com>
2025-11-05 20:15:29 +0000

    Update project prj_usg0bshx - 2025-11-05T20:15:28.696Z

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import GradientBackground from './components/GradientBackground'
+import Footer from './components/Footer'
 import Home from './pages/Home'
 import Tracker from './pages/Tracker'
 import History from './pages/History'
@@ -9,15 +10,18 @@ import About from './pages/About'
 function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen relative">
+      <div className="min-h-screen relative flex flex-col">
         <GradientBackground />
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/tracker" element={<Tracker />} />
-          <Route path="/history" element={<History />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
+        <div className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/tracker" element={<Tracker />} />
+            <Route path="/history" element={<History />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
     </BrowserRouter>
   )
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="mt-16 py-8 text-center text-sm text-white/70">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <p>© {new Date().getFullYear()} Shakti Alert • Built with care for safety</p>
+      </div>
+    </footer>
+  )
+}
